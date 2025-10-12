c2ec69c77fcfad3ec7afea80556ea5e90d64a30a
Zack <gamasekk.er@mail.ru>
2025-10-12 23:31:43 +0300

    проба пера

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
 module bring_some_water_please
 
-go 1.25
 
-require (
-    github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-)
+go 1.25.2
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
--- internal/bot/Bot.go
+++ internal/bot/Bot.go
@@ -1,1 +1,60 @@
-package bot
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+
+	token := os.Getenv("huita")
+	if token == "" {
+
+		log.Fatal("Ты не поставил токен бота. Его можно узнать у @g4mace")
+
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+
+			switch update.Message.Command() {
+
+			case "start":
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "куку ёпта)_)))")
+				bot.Send(msg)
+
+			default:
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "пошёл нахуй пидорас нормальную команду напиши")
+				bot.Send(msg)
+
+			}
+
+		} else {
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не, чувак, я не "+update.Message.Text)
+			bot.Send(msg)
+
+		}
+
+	}
+
+}
