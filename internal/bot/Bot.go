82ff7539f1975fa9631e2a6ee69d13a481e75650
Zack <gamasekk.er@mail.ru>
2025-10-14 15:37:02 +0300

    Update Bot.go

--- internal/bot/Bot.go
+++ internal/bot/Bot.go
@@ -1,60 +1,40 @@
-package main
+package tgbot
 
 import (
 	"log"
-	"os"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	botApi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-func main() {
+func tgbot() {
 
-	token := os.Getenv("huita")
-	if token == "" {
+	// токен бота
+	var tokenBot string = "7833805808:AAHo8zQ_VT2tkbrbwu2-ud0OKzdkgDovpCs"
 
-		log.Fatal("Ты не поставил токен бота. Его можно узнать у @g4mace")
-
-	}
-
-	bot, err := tgbotapi.NewBotAPI(token)
+	// объект бота + ошибки при подключении
+	bot, err := botApi.NewBotAPI(tokenBot)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	//обновления в чате
+	checking := botApi.NewUpdate(0)
+	checking.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+	// канал с обновлениями идёт из bot в updates
+	updateChannel, err := bot.GetUpdatesChan(checking)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	for update := range updates {
+	//цикл с приёмом обновлений из канала
+	for update := range updateChannel {
 		if update.Message == nil {
 			continue
 		}
 
-		if update.Message.IsCommand() {
-
-			switch update.Message.Command() {
-
-			case "start":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "куку ёпта)_)))")
-				bot.Send(msg)
-
-			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "пошёл нахуй пидорас нормальную команду напиши")
-				bot.Send(msg)
-
-			}
-
-		} else {
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не, чувак, я не "+update.Message.Text)
-			bot.Send(msg)
-
-		}
-
+		msg := botApi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		bot.Send(msg)
 	}
 
 }
