e114778e9e4a7d09589cdca1d8acf8e5fd800bee
Mikstyz <ehouz85@gmail.com>
2025-10-21 20:29:40 +0300

    Сменил имя для функции создания таблиц, так же добавил функцию для удаления таблиц, так же немного поменял логирование

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -14,7 +14,7 @@ func NewMigrate(db *sql.DB) *Migrate {
 	return &Migrate{db: db}
 }
 
-func (r *Migrate) Run() error {
+func (r *Migrate) CreateTables() error {
 
 	tables := []string{
 		mData.UsersTable,        //пользователи
@@ -36,8 +36,45 @@ func (r *Migrate) Run() error {
 			return err
 		}
 
-		log.Print("[DATA BASE][MIGRATE] - Успешное создание таблицы")
+		log.Print("[SQL][MIGRATE] - Успешное создание таблицы")
 	}
 
+	return nil
+}
+func (r *Migrate) DropAllTables() error {
+	const selectTablesQuery = `
+	SELECT name FROM sqlite_master
+	WHERE type='table' AND name NOT LIKE 'sqlite_%';
+	`
+
+	rows, err := r.db.Query(selectTablesQuery)
+	if err != nil {
+		log.Fatalf("[SQL][Drop All Tables] Ошибка при получении списка таблиц: %v", err)
+		return err
+	}
+	defer rows.Close()
+
+	var tableName string
+	var tables []string
+
+	for rows.Next() {
+		if err := rows.Scan(&tableName); err != nil {
+			log.Fatalf("[SQL][Drop All Tables] Ошибка при сканировании: %v", err)
+			return err
+		}
+		tables = append(tables, tableName)
+	}
+
+	removedTables := 0
+	for _, t := range tables {
+		if _, err := r.db.Exec("DROP TABLE IF EXISTS " + t); err != nil {
+			log.Fatalf("[SQL][Drop All Tables] Ошибка при удалении таблицы %s: %v", t, err)
+			return err
+		}
+		removedTables++
+		log.Printf("[SQL][Drop All Tables] Таблица удалена: %s", t)
+	}
+
+	log.Printf("[SQL][Drop All Tables] Успешное удаление таблиц. Всего удалено: %d", removedTables)
 	return nil
 }
