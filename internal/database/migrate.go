abeae6ce8b4c4a364e48842b32bfe7b3ade7d96e
Mikstyz <ehouz85@gmail.com>
2025-10-21 20:14:11 +0300

    Создал функцию для создания таблиц в бд

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	mData "bring_some_water_please/internal/database/Migrationsdata"
 	"database/sql"
+	"log"
 )
 
 type Migrate struct {
@@ -12,10 +14,30 @@ func NewMigrate(db *sql.DB) *Migrate {
 	return &Migrate{db: db}
 }
 
-func CreateTable(r *Migrate) {
+func (r *Migrate) Run() error {
 
-}
+	tables := []string{
+		mData.UsersTable,        //пользователи
+		mData.ModsTable,         //Моды
+		mData.VersionsTable,     //версии
+		mData.FilesTable,        //файлы модов url
+		mData.LoadersTable,      //ядра модов
+		mData.AssembliesTable,   //сборки
+		mData.AssemblyModsTable, //моды в сборках
+	}
 
-func RemoveTable(r *Migrate) {
+	for _, query := range tables {
+		if _, err := r.db.Exec(query); err != nil {
+			log.Printf(`Ошибка при выполнении запроса:
+				Error: %v
+				Query: %s
+				`, err, query)
 
+			return err
+		}
+
+		log.Print("[DATA BASE][MIGRATE] - Успешное создание таблицы")
+	}
+
+	return nil
 }
